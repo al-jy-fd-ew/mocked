6ee49700571f58e9800baf20b861eefb83615c21
finleydecker <finleydecker@gmail.com>
2022-09-26 15:22:29 -0700

    basic algo section is set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import AlgoSection from './components/algo.jsx';
 import '../src/styling.scss';
 
 const App = () => (
   <div>
-        Hello
+    <AlgoSection/>
   </div>
 );
 
--- src/components/algo.jsx
+++ src/components/algo.jsx
@@ -1,2 +1,39 @@
 import React from 'react';
+import AceEditor from 'react-ace';
+import 'ace-builds/src-noconflict/mode-javascript';
+import 'ace-builds/src-noconflict/ext-language_tools';
+import 'ace-builds/src-noconflict/ext-beautify';
 
+const AlgoSection = () => /* const [code, setCode] = React.useState(*/ /*   ''*/ /* );*/ (
+  <div id='algo-section'>
+    <h3>Question</h3>
+    <p>Given an array of integers nums and an integer target, return indices of the two numbers such
+      that they add up to target. You may assume that each input would have exactly one solution, and
+      you may not use the same element twice. You can return the answer in any order.</p>
+    <AceEditor
+      style={{
+        height: '50vh',
+        width: '80%',
+      }}
+      // placeholder='Start Coding'
+      mode='javascript'
+      theme='github'
+      name='basic-code-editor'
+      // onChange={currentCode => setCode(currentCode)}
+      fontSize={14}
+      showPrintMargin={false}
+      showGutter={true}
+      highlightActiveLine={true}
+      // value={code}
+      setOptions={{
+        // enableBasicAutocompletion: true,
+        // enableLiveAutocompletion: true,
+        enableSnippets: true,
+        showLineNumbers: true,
+        tabSize: 4,
+      }} />
+    <button>Go to next section</button>
+  </div>
+);
+
+export default AlgoSection;
