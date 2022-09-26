4649be78a00c0531776ff82692a11b4abeba0c7f
Alex Law <alexlaw528@gmail.com>
2022-09-26 16:12:38 -0700

    Add additional boilerplate setup for backend.
    Set up hooks to postgresql server.
    
    Started create.sql file.

--- server/server.js
+++ server/server.js
@@ -1,17 +1,33 @@
 const path = require('path');
 const express = require('express');
 
-// const dotenv = require('dotenv');
-// dotenv.config();
-
-// Import controllers
-
+const dotenv = require('dotenv');
+dotenv.config();
 
 const PORT = 3000;
 const app = express();
 
+// Import controllers
+app.use(express.json()); // recognize incoming request as Json Object
+app.use(express.urlencoded({ extended: true })); // parse incoming string or array request
+
+// serve static assets
+app.use('/src', express.static(path.resolve(__dirname, '../src')));
+app.use('/dist', express.static(path.resolve(__dirname, '../dist')));
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
+});
+
+// global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error hath occurred!' },
+  };
+  const errorObj = Object.assign(defaultErr, err);
+  return res.status(errorObj.status).send(errorObj.msg);
 });
 
 module.exports = app;
