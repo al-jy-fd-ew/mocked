23914f58077c90d219b8a536db1080fd264bf14f
janeyou94 <janeyou94@gmail.com>
2022-09-26 19:36:23 -0400

    behavioral questions component, auth component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import '../src/styling.scss';
+import Login from './components/log-in';
+import Signup from './components/sign-up';
+import Behavioral from './components/behavioral';
 
 const App = () => (
   <div>
-        Hello
+        <Login />
+        <Signup />
+        <Behavioral />
+        {/* just appended everything to app for now to visualize how each section looks */}
+
   </div>
 );
 
---
+++ src/components/behavioral.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const sampleQuestions = [
+  'Tell me about a time when you performed well under enormous pressure.',
+  'Tell me about a time you had a conflict at work',
+  'Can you detail a mistake you made and how you reacted to it?',
+  'Have you ever had a disagreement with an immediate supervisor?',
+  'Describe a time when you went above and beyond.',
+]; // array for now but we can turn into object later to store questions keys in our database
+
+function getRandomInt() {
+  return Math.floor(Math.random() * 5);
+}
+
+const Behavioral = () => {
+  return (
+
+    <div id="behavioral">
+      <h1>Mock Interview App</h1>
+      {/* {(isLoggedIn === true) && <Navigate to="/" replace={true} />} */}
+      <h3>Behavioral Question</h3>
+      <form onSubmit={(e) => verifyUser(e)}>
+        <p>{sampleQuestions[getRandomInt()]}</p>
+        <textarea name="answer" type="text" placeholder="Type out your response here..." style={{height:400, width:600}}></textarea>
+        <br></br>
+        <input type='submit' value="Go to the Next Section"></input>
+      </form>
+    </div>
+  )
+}
+
+export default Behavioral;
