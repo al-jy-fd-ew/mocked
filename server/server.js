3b146919a4b0f4632be852ad66f243df30023d0f
finleydecker <finleydecker@gmail.com>
2022-09-27 11:29:39 -0700

    setting up server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,27 @@
 const path = require('path');
 const express = require('express');
+const userController = require('user')
 
 // const dotenv = require('dotenv');
 // dotenv.config();
 
-// Import controllers
-
-
 const PORT = 3000;
 const app = express();
+
+app.use(express.json()); // recognize incoming request as Json Object
+app.use(express.urlencoded({ extended: true })); // parse incoming string or array request
+
+app.use('/src', express.static(path.resolve(__dirname, '../src')));
+
+app.post('/signup', userController.createUser, (req, res) => {
+
+});
+
+app.post('/login', userController.verifyUser, (req, res) => {
+
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
