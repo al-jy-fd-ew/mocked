a95fe22b358c9c446ce47edf854beb6c7af0395a
finleydecker <finleydecker@gmail.com>
2022-09-28 11:39:21 -0700

    added basic styling to login and sign up

--- src/pages/log-in.jsx
+++ src/pages/log-in.jsx
@@ -32,11 +32,11 @@ const Login = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+    const username = document.getElementsByClassName('username').value;
+    const password = document.getElementsByClassName('password').value;
 
-    document.getElementById('username').value = '';
-    document.getElementById('password').value = '';
+    document.getElementsByClassName('username').value = '';
+    document.getElementsByClassName('password').value = '';
 
     fetch('/api/login', {
       method: 'POST',
@@ -50,19 +50,21 @@ const Login = () => {
 
   return (
 
-    <div id='login'>
+    <div className='credentials'>
       <h1>Mock Interview App</h1>
       <h3>Log In</h3>
       <form method='POST' onSubmit={handleSubmit}>
-        <input id='username' name='username' type='text' placeholder='Username'></input>
-        <br></br>
-        <input id='password' name='password' type='password' placeholder='Password'></input>
-        <br></br>
-        <input type='submit' value='Login'></input>
+        <p>Username</p>
+        <input className='username' name='username' type='text'></input>
+        <p>Password</p>
+        <input className='password' name='password' type='password'></input>
+        <input className='submit' type='submit' value='Login'></input>
       </form>
+      <p>Don&apos;t have an account?</p>
       <Link to='/signup'>
-        <button>Don&apos;t have an account? Sign up here.</button>
+        <button>Sign Up</button>
       </Link>
+      <hr />
       <div id='signInDiv'></div>
     </div>
   );
